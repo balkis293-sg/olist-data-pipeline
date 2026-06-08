0a209211bba320ba1fe239e750f733c120981ffd
oasisgem <kolzoasis@gmail.com>
2026-06-08 22:43:58 +0800

    Added Jupyter Notebook documentation for Phase 2 Star Schema

--- dbt_olist/models/marts/fact_orders.sql
+++ dbt_olist/models/marts/fact_orders.sql
@@ -4,13 +4,30 @@
 
 WITH staging_order_items AS (
     SELECT * FROM {{ ref('stg_order_items') }}
+),
+
+staging_orders AS (
+    SELECT * FROM {{ ref('stg_orders') }}
 )
 
 SELECT
-    order_item_id AS order_item_sk,    -- Surrogate Key
-    order_id AS order_key,             -- Links to an order dimension if you have one
-    product_id AS product_key,         -- Links to dim_products
-    seller_id AS seller_key,           -- Links to dim_sellers
-    price,
-    freight_value
-FROM staging_order_items
+    -- Primary Key for the Fact Table
+    i.order_item_id AS order_item_sk,    
+    
+    -- Foreign Keys connecting to your Dimensions
+    i.order_id AS order_key,             
+    i.product_id AS product_key,         
+    i.seller_id AS seller_key,           
+    o.customer_id AS customer_key,       
+    
+    -- Formatting the timestamp into a string 'YYYYMMDD' to match dim_date
+    CAST(FORMAT_DATE('%Y%m%d', DATE(o.order_purchase_timestamp)) AS STRING) AS date_key,
+    
+    -- Your Facts (Measurable Numbers)
+    i.price,
+    i.freight_value
+
+FROM staging_order_items i
+-- We join the orders table to bring in the customer and date information!
+LEFT JOIN staging_orders o 
+    ON i.order_id = o.order_id
